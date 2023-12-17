e091975348ab31a13d5d7178e170eafde4d94e19
Denis Olivier <hello@olivierdenis.be>
2023-12-17 18:10:20 +0100

    salt password seeder

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,17 +1,22 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
-const { faker } = require('@faker-js/faker') // Mise à jour de l'importation
+const { faker } = require('@faker-js/faker')
+const bcrypt = require('bcryptjs')
 
 async function main() {
   const numberOfUsers = 10
 
   for (let i = 0; i < numberOfUsers; i++) {
+    const rawPassword = faker.internet.password()
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(rawPassword, salt)
+
     await prisma.user.create({
       data: {
         firstname: faker.person.firstName(),
         lastname: faker.person.lastName(),
         email: faker.internet.email(),
-        password: faker.internet.password(),
+        password: hashedPassword,
         language: 'en',
       },
     })
