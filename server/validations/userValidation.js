9cd4eb70f6b029b14b930ecbc152d43532a10819
Denis Olivier <hello@olivierdenis.be>
2024-01-27 23:57:12 +0100

    user validation update

--- server/validations/userValidation.js
+++ server/validations/userValidation.js
@@ -1,11 +1,24 @@
 const Joi = require('joi')
 
-const userValidationSchema = Joi.object({
+const user = Joi.object({
   firstname: Joi.string().min(2).max(30).required(),
   lastname: Joi.string().min(2).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   language: Joi.string().valid('en', 'fr', 'es').required(),
 })
+
+const editUser = Joi.object({
+  uuid: Joi.string().required(),
+  firstname: Joi.string().min(2).max(30),
+  lastname: Joi.string().min(2).max(30),
+  email: Joi.string().email(),
+  language: Joi.string().valid('en', 'fr', 'es'),
+})
+
+const userValidationSchema = {
+  user,
+  editUser,
+}
 
 module.exports = userValidationSchema
