337818189fcc3e6e05d3869039a60b972ff34f76
Denis Olivier <hello@olivierdenis.be>
2024-01-27 03:53:08 +0100

    token validation

--- server/models/tokens.js
+++ server/models/tokens.js
@@ -7,6 +7,18 @@ const tokenModel = {
       data: tokenData,
     })
   },
+
+  findToken: async (token) => {
+    return await prisma.token.findUnique({
+      where: { token: token },
+    })
+  },
+
+  deleteToken: async (token) => {
+    return await prisma.token.delete({
+      where: { token: token },
+    })
+  },
 }
 
 module.exports = tokenModel
