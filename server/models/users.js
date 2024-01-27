a416f9b30a62b9238f071b127f29983c7eb9e5c7
Denis Olivier <hello@olivierdenis.be>
2024-01-27 03:56:18 +0100

    add updateUserVerification to usersModel

--- server/models/users.js
+++ server/models/users.js
@@ -5,6 +5,7 @@ const UserModel = {
   users: async () => {
     return await prisma.user.findMany()
   },
+
   user: async (uuid) => {
     return await prisma.user.findUnique({
       where: { uuid: uuid },
@@ -14,6 +15,13 @@ const UserModel = {
   createUser: async (userData) => {
     return await prisma.user.create({
       data: userData,
+    })
+  },
+
+  updateUserVerification: async (userId, isVerified) => {
+    return await prisma.user.update({
+      where: { id: userId },
+      data: { isVerified: isVerified },
     })
   },
 }
