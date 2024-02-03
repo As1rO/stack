48c24e4868e5d0deeadc71054e40a277dd1a0183
Denis Olivier <hello@olivierdenis.be>
2024-02-03 15:33:57 +0100

    clean

--- plugins/apollo-error.js
+++
@@ -1,9 +0,0 @@
-import { removeJwtToken } from '../utils/local-storage'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('apollo:error', (error) => {
-    if (error?.networkError?.statusCode === 401) {
-      removeJwtToken()
-    }
-  })
-})
