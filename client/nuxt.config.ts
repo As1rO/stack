52b861c9568c8966ca269d0f909ad321c71d73a4
Denis Olivier <hello@olivierdenis.be>
2024-03-03 14:44:02 +0100

    add plugin pinia

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -25,7 +25,7 @@ export default async function defineNuxtConfig() {
         apiUrl: ENV.API_URL,
       },
     },
-    modules: ['@nuxtjs/apollo', '@nuxt/ui'],
+    modules: ['@nuxtjs/apollo', '@nuxt/ui', '@pinia/nuxt'],
     apollo: {
       clients: {
         default: {
