1b38230011dd60e1cb020a9a4f961fa097f83e32
Denis Olivier <hello@olivierdenis.be>
2024-03-10 23:30:00 +0100

    seeder

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -7,20 +7,51 @@ async function main() {
   const numberOfUsers = 10
 
   for (let i = 0; i < numberOfUsers; i++) {
-    // const rawPassword = faker.internet.password()
     const rawPassword = 'test'
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(rawPassword, salt)
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
-        firstname: faker.person.firstName(),
-        lastname: faker.person.lastName(),
+        firstname: faker.name.firstName(),
+        lastname: faker.name.lastName(),
         email: faker.internet.email(),
         password: hashedPassword,
         language: 'en',
       },
     })
+
+    // Créer un compte pour chaque utilisateur
+    const account = await prisma.account.create({
+      data: {
+        user_id: user.id,
+        name: faker.finance.accountName(),
+        currency: 'EUR',
+      },
+    })
+
+    // Créer des transactions pour le compte
+    const numberOfTransactions = 15
+    for (let j = 0; j < numberOfTransactions; j++) {
+      await prisma.transaction.create({
+        data: {
+          account_id: account.id,
+          amount: parseFloat(faker.finance.amount()),
+          status: 'completed',
+          transaction_date: faker.date.recent(),
+          transaction_type: 'debit',
+          description: faker.finance.transactionDescription(),
+          currency: 'EUR',
+          payment_method: 'credit card',
+          ...(faker.datatype.boolean() && {
+            external_transaction_id: faker.datatype.uuid(),
+          }),
+          ...(faker.datatype.boolean() && {
+            refund_id: faker.datatype.number(),
+          }),
+        },
+      })
+    }
   }
 }
 
